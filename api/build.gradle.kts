f3682ed5b5559c9ccf3986d2471124f055fb778e
Simple <L834787411@outlook.com>
2024-02-21 11:18:10 +0800

    publish update

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,8 +1,28 @@
 plugins {
     id("geyser.publish-conventions")
+    id("maven-publish")
 }
 
 dependencies {
     api(libs.base.api)
     api(libs.math)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/StarsRealm/Packages")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create("gpr", MavenPublication::class.java) {
+            from(components.getByName("java"))
+            groupId = project.group.toString()
+        }
+    }
 }
