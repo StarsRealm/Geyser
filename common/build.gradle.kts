391bde4f1aff0640b725ae0c4ad113a50fe28eeb
Simple <L834787411@outlook.com>
2024-04-11 10:00:21 +0800

    feat: common publish

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("geyser.publish-conventions")
+    id("maven-publish")
 }
 
 dependencies {
@@ -10,5 +11,32 @@ dependencies {
 indra {
     javaVersions {
         target(8)
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "AliYun-Release"
+            url = uri("https://packages.aliyun.com/maven/repository/2421751-release-ZmwRAc/")
+            credentials {
+                username = project.findProperty("aliyun.package.user") as String? ?: System.getenv("ALY_USER")
+                password = project.findProperty("aliyun.package.password") as String? ?: System.getenv("ALY_PASSWORD")
+            }
+        }
+        maven {
+            name = "AliYun-Snapshot"
+            url = uri("https://packages.aliyun.com/maven/repository/2421751-snapshot-i7Aufp/")
+            credentials {
+                username = project.findProperty("aliyun.package.user") as String? ?: System.getenv("ALY_USER")
+                password = project.findProperty("aliyun.package.password") as String? ?: System.getenv("ALY_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create("gpr", MavenPublication::class.java) {
+            from(components.getByName("java"))
+            groupId = project.group.toString()
+        }
     }
 }
